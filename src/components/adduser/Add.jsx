d4ba011998cd6d4c3efab0fb7c263c73a2b3659f
Abhishek Jaiswal <jaisawalabhi0102@gmail.com>
2024-04-10 16:17:04 +0530

    first commit

---
+++ src/components/adduser/Add.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import './Add.css'
+import axios from 'axios'
+import toast from 'react-hot-toast';
+import { Link , useNavigate} from 'react-router-dom'
+const Add = () => {
+      const users = {
+            fname:"",
+            lname:"",
+            email:"",
+            password:""
+      }
+      const [user,setUser] = useState(users);
+
+      const navigate = useNavigate();
+
+
+      const inputHandler = (e) =>{
+            const {name,value} = e.target;
+            setUser({...user,[name]:value})
+            // console.log(user);
+            // console.log(value);
+      }
+
+
+      const submitForm = async(e) =>{
+            e.preventDefault();
+            await axios.post("http://localhost:8000/api/create",user)
+            .then((response)=>{
+                  // console.log(response);
+                  toast.success(response.data.msg, {position:"top-right"})
+                  navigate("/")
+            }).catch(error => console.log(error))
+      }
+
+
+  return (
+    <div className='addUser'>
+      <Link to={'/'}>Back</Link>
+      <h3>Add New user</h3>
+      <form  className='addUserForm' onSubmit={submitForm}>
+            <div className="inputGroup">
+                  <label htmlFor="fname">First name</label>
+                  <input type="text" onChange={inputHandler} id='fname' name='fname' autoComplete='off' placeholder='enter your first name' />
+            </div>
+            <div className="inputGroup">
+                  <label htmlFor="lname">Last name</label>
+                  <input type="text" onChange={inputHandler} id='lname' name='lname' autoComplete='off' placeholder='enter your last name' />
+            </div>
+            <div className="inputGroup">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" onChange={inputHandler} id='email' name='email' autoComplete='off' placeholder='enter your Email' />
+            </div>
+            <div className="inputGroup">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" onChange={inputHandler} id='password' name='password' autoComplete='off' placeholder='enter your Password' />
+            </div>
+            <div className="inputGroup">
+                  <button type='submit'>Add User</button>
+            </div>
+      </form>
+    </div>
+  )
+}
+
+export default Add
---
+++ src/components/updateuser/Edit.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate,useParams } from 'react-router-dom'
+import "../adduser/Add.css"
+import axios from "axios";
+import toast from 'react-hot-toast'
+const Edit = () => {
+
+      const users = {
+            fname:"",
+            lname:"",
+            email:""
+      }
+
+      const {id} = useParams();
+      const navigate = useNavigate();
+      const [user,setUser] = useState(users);
+      
+
+      const inputChangeHandler = (e) =>{
+            const {name,value} = e.target;
+            setUser({...user,[name]:value})
+            console.log(user);
+      }
+
+      useEffect(()=>{
+            axios.get(`http://localhost:8000/api/getone/${id}`)
+            .then((response)=>{
+                  // console.log(response);
+                  setUser(response.data)
+            }).catch((error)=>{
+                  console.log(error);
+            })
+      },[id])
+
+      const submitForm  = async(e)=>{ 
+            e.preventDefault();
+            await axios.put(`http://localhost:8000/api/update/${id}`,user)
+            .then((response)=>{
+                  // console.log(response);
+                  toast.success(response.data.msg, {position:"top-right"})
+                  navigate("/")
+            }).catch(error => console.log(error))
+      }
+  return (
+      <div className='addUser'>
+      <Link to={'/'}>Back</Link>
+      <h3>Update user</h3>
+      <form  className='addUserForm' onSubmit={submitForm}>
+            <div className="inputGroup">
+                  <label htmlFor="fname">First name</label>
+                  <input type="text" value={user.fname} onChange={inputChangeHandler} id='fname' name='fname' autoComplete='off' placeholder='enter your first name' />
+            </div>
+            <div className="inputGroup">
+                  <label htmlFor="lname">Last name</label>
+                  <input type="text" value={user.lname} onChange={inputChangeHandler} id='lname' name='lname' autoComplete='off' placeholder='enter your last name' />
+            </div>
+            <div className="inputGroup">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" value={user.email} onChange={inputChangeHandler} id='email' name='email' autoComplete='off' placeholder='enter your Email' />
+            </div>
+            <div className="inputGroup">
+                  <button type='submit'>Update User</button>
+            </div>
+      </form>
+    </div>
+  )
+}
+
+export default Edit
